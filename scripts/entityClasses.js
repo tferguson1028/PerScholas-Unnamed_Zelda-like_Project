2ecd04134bc07483363845c15f9f6cbf7700f5e6
tferguson1028 <tferguson1028@outlook.com>
2023-11-07 01:52:29 -0500

    Setting up classes

--- scripts/entityClasses.js
+++ scripts/entityClasses.js
@@ -22,12 +22,74 @@ class Entity
     this.xPos += Number(deltaX);
     this.yPos += Number(deltaY);
   }
-
+  
+  getCollisionBox()
+  {
+    // https://stackoverflow.com/a/63419039
+    return this.linkedHTMLElement.getBoundingClientRect();
+  }
+  
   /**
-   * This method updates the CSS based on the Entity's internal variables.
+   * Holds logic for updating entities. Is empty in base entity class.
+   * @param {Number} deltaTime 
+   */
+  process(deltaTime) {}
+  
+  /**
+   * Updates the CSS based on the Entity's internal variables.
    */
   update() 
   {
     this.linkedHTMLElement.style.translate = `${this.xPos}px ${this.yPos}px`;
+  }
+}
+
+class Interactable extends Entity
+{
+  
+}
+
+/**
+ * These are used to create attacks and damage. 
+ * All hitboxes should be projectile class.
+ * They will persist until their lifetime is over, then it will
+ * delete itself.
+ */
+class Projectile extends Entity
+{
+  lifeTime = 0;
+  speedX = 0;
+  speedY = 0;
+  owner = null;
+  
+  process(deltaTime)
+  {
+    if(this.lifeTime <= 0) { this.destroy(); return; }
+    
+    this.lifeTime -= deltaTime;
+    this.translate(this.speedX*deltaTime, this.speedY*deltaTime);
+  }
+  
+  startTraverse(speedX, speedY, lifeTime)
+  {
+    this.lifeTime = lifeTime;
+    this.speedX = speedX;
+    this.speedY = speedY;
+  }
+  
+  destroy() {}
+}
+
+
+/**
+ * These are entities that are controllable. 
+ */
+class Actor extends Entity
+{
+  constructor(htmlElement, spriteSheet, initX, initY, sizeX, sizeY, health)
+  {
+    super(htmlElement, spriteSheet, initX, initY, sizeX, sizeY);
+    this.maxHealth = health;
+    this.health = health;
   }
 }
